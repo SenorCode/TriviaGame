68d02f24a83494bc4b147007d3b2365c732e5785
SenorCode <appmar23@gmail.com>
2018-03-07 22:21:24 -0500

    made minor changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,27 +22,67 @@ $(document).ready(function () {
 	var randomQuestion = Questions[Math.floor(Math.random()*3)];
 	var count = 0;
 	var time = 10;
-	var correctAnswer;
-	var incorrectAnswer;
+	var correctAnswer = 0;
+	var incorrectAnswer = 0;
 	var unansweredQuestions;
-	console.log(randomQuestion);
-$("#startButton").on('click', function(){
-
-	$(this).hide()
-		quizStart();
-});
+	var correctAnswerIndex = randomQuestion.correctAnswer;
 	
+
+	$("#startButton").on('click', function(){
+
+		$(this).hide()
+		$("#title").hide();
+		quizStart();
+	});
+		console.log(randomQuestion);
+
+		//console.log(randomQuestion.question);
+		//console.log(typeof randomQuestion.question);
+		//console.log(randomQuestion.answers);
+		//console.log(typeof randomQuestion.answers);
+		//console.log(randomQuestion.answers[correctAnswerIndex]);
+		//console.log(typeof randomQuestion.answers[correctAnswerIndex]);
+
+
 
 	function quizStart() {
+		buttontext = randomQuestion.answers;
+		$("#question").html("<h2>" + randomQuestion.question + "</h2>");
+		for(var i = 0; i < Questions.length; i++){
+			var answerButtons = $("<button>" + buttontext[i] +"</button>").attr(
+												{
+													class: "btn btn-default btn-lg",
+													type: "button",
 
-	$("#question").html("<h2>" + randomQuestion.question + "</h2>").pop();
-
-	
-	
+											
+												}).css("width", "350px");
+				
+			answerButtons.appendTo("#answers");
+		}//end of for loop
 		
-	}
+		$(".btn").click( function() {
+			
+				var x= $(this).text();
+
+				console.log(x);
+
+				if(x === randomQuestion.answers[correctAnswerIndex]){
+					correctAnswer++;
+					newQuestion()
 
 
+				}else {
+					incorrectAnswer++;
+					newQuestion()
+				}
+
+				
+			
+		})
+			
+	}// end of quizStart()
+
+	function newQuestion(){}
 
 
 
